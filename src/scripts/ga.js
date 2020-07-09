1f07b0bb43ca6618de43a00edcbb3a10683ea56a
hipertextos <hipertextos@gmail.com>
2020-07-09 17:58:02 +0200

    Update ga.js

--- src/scripts/ga.js
+++ src/scripts/ga.js
@@ -4,6 +4,5 @@
   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
   })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-  ga('create', 'UA-9844180-1', 'auto');
+  ga('create', 'UA-172147666-1', 'auto');
   ga('send', 'pageview');
-
